a78ef653b103a69c6c7a44b9747dfd120b30e85b
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-04 16:29:43 +0100

    Aggiunto input e controllo sull'input

--- js/script.js
+++ js/script.js
@@ -1,18 +1,54 @@
-const list =['cioccolata', 'guanciale', 'pecorino', 'pasta', 'uova','passata di pomodoro', 'piatti', 'acqua']
+const list =['pecorino', 'pasta' , 'uova','passata ', 'acqua']
 const list_container = document.querySelector('.shopping-list')
-
 let item = 0;
-console.log(list.length)
+let item_add = 0;
+let item_up = 0;
+let spesa_item
+let count = 0;
+
+let button = document.getElementById('item_up')
+button.addEventListener('click', function(){
+
+    spesa_item = document.getElementById("item_add").value; 
+    
+    if (spesa_item == "") {
+        alert("Inserisci un'elemento");
+        return false;
+      }
+    
+    else{
+
+    count = list.push(spesa_item);
+    
+    while( item < list.length){
+    
+        let list_item = document.createElement('li');
+    
+        list_item.innerText = list[item];
+    
+        list_item.classList.add('list-class', 'animate__headShake');
+    
+        list_container.append(list_item);
+        
+        item++
+    }
+}
+    
+})
 
 while( item < list.length){
+  
+      let list_item = document.createElement('li');
+  
+      list_item.innerText = list[item];
+  
+      list_item.classList.add('list-class', 'animate__headShake');
+  
+      list_container.append(list_item);
+      
+      item++
 
-    let list_item = document.createElement('li');
+  }
 
-    list_item.innerText = list[item];
 
-    list_item.classList.add('list-class', 'animate__headShake');
 
-    list_container.append(list_item);
-
-    item++
- }
